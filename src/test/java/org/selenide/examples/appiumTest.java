df0b22b546edb3b267e0e4abab0d20c131400633
Nguyễn Thành Thoại Long (VINID-KSP-MN) <longntt1@vingroup.net>
2019-10-04 18:17:55 +0700

    my-commit

--- src/test/java/org/selenide/examples/appiumTest.java
+++ src/test/java/org/selenide/examples/appiumTest.java
@@ -34,6 +34,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class appiumTest {
     private AppiumDriver<WebElement> driver;
@@ -43,14 +44,17 @@ public class appiumTest {
     @Before
     public void setUp() throws Exception {
         // set up appium
-        File appDir = new File("D:\\Working\\Appium\\test");//System.getProperty("user.dir"), "D:\\Working\\Appium\\test");
-        File app = new File(appDir, "VinID_v5.0.0.apk");
+        File appDir = new File("D:\\Vinhome\\apk");//System.getProperty("user.dir"), "D:\\Working\\Appium\\test");
+        File app = new File(appDir, "app-uat-release.apk");
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformVersion", "5");
-        capabilities.setCapability("deviceName", "Test");
+        capabilities.setCapability("platformVersion", "10");
+        capabilities.setCapability("deviceName", "emulator-5554");
         capabilities.setCapability("app", app.getAbsolutePath());
+        capabilities.setCapability("appPackage", "com.vingroup.vinid.uat");
+        capabilities.setCapability("appActivity", "com.vingroup.vinid.ui.gateway.GatewayActivity");
         driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         values = new ArrayList<>();
+        WebDriverWait wait;
     }
 
     @After
@@ -61,10 +65,50 @@ public class appiumTest {
     @Test
     public void testBasicAlert() throws Exception {
         Assert.assertTrue(true);
-        //driver.findElement(By.xpath("//UIAButton[2]")).click();
-        //Alert alert = driver.switchTo().alert();
-        //check if title of alert is correct
-        //assertEquals("Cool title this alert is so cool.", alert.getText());
-        //alert.accept();
+        driver.findElement(By.id("btnAction")).click();
+        /**The account has sync*/
+        //driver.findElement(By.id("etContent")).sendKeys("0115272727");
+        /** The account has more than 2 apartments*/
+        //driver.findElement(By.id("etContent")).sendKeys("0115123456");
+        /** The account has no apartment*/
+        driver.findElement(By.id("etContent")).sendKeys("0115222333");
+
+        driver.findElement(By.id("btnNext")).click();
+        driver.findElement(By.id("etContent")).sendKeys("123456");
+        driver.findElement(By.id("btnNext")).click();
+        driver.findElement(By.id("itemPin")).sendKeys("112233");
+        //driver.findElement(By.id("btnNext")).click();
+        driver.findElements(By.id("ivStaticIcon")).get(1).click();
+        //check if button Tôi là cư dân Vinhomes presents
+        Assert.assertTrue(driver.findElement(By.id("btnContinues")).isDisplayed());
+        driver.findElement(By.id("btnContinues")).click();
+        //check if page welcome resident is displayed
+        Assert.assertTrue(driver.findElement(By.id("svWelcome")).isDisplayed());
+        driver.findElement(By.id("btnContinues")).click();
+        Assert.assertTrue(driver.findElement(By.id("toolbarTitle")).getText().contains("Thông tin cư dân"));
+        driver.findElement(By.id("etContent")).sendKeys("0115272727");
+        driver.findElement(By.id("btnContinues")).click();
+
+        /**Verify that the page Chọn khu đô thị display*/
+        Assert.assertTrue(driver.findElement(By.id("tvTitle")).getText().contains("Bạn đang ở khu đô thị nào?"));
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Vinhomes Golden River']")).click();
+
+        /**Verify that the page Chọn tòa nhà display*/
+        Assert.assertTrue(driver.findElement(By.id("tvTitle")).getText().contains("Bạn đang ở tòa nhà/khu nào?"));
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Aqua 03']")).click();
+
+        /**Verify that the page Chọn tầng/khu biệt thự display*/
+        Assert.assertTrue(driver.findElement(By.id("tvTitle")).getText().contains("Bạn đang ở tầng/khu biệt thự nào?"));
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Tầng 03']")).click();
+
+        /**Verify that the page Chọn căn hộ/biệt thự display*/
+        Assert.assertTrue(driver.findElement(By.id("tvTitle")).getText().contains("Bạn đang ở căn hộ/biệt thự nào?"));
+        driver.findElement(By.xpath("//android.widget.TextView[@text='A030307']")).click();
+
+        /**Verify that the page Xác nhận thông tin display*/
+        Assert.assertTrue(driver.findElement(By.id("tvTitle")).getText().contains("Xác nhận thông tin"));
+        driver.findElement(By.id("tvConfirm")).click();
+
+        Thread.sleep(2000);
     }
 }
